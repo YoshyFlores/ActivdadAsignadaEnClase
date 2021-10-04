ffa948e211d432c8e393558c910b221a59f1d360
Cesar Martinez <oliver-cesar@hotmail.com>
2021-10-03 22:28:26 -0600

    Creando objetos dentro del form y pasandole los parametros a mostrar en la caja de texto.

--- Animales/Form1.Designer.cs
+++ Animales/Form1.Designer.cs
@@ -29,13 +29,37 @@ namespace Animales
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(651, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informacion de Los Animales";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Animales";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Animales/Form1.cs
+++ Animales/Form1.cs
@@ -16,5 +16,58 @@ namespace Animales
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //Felino
+            Leon leon = new Leon();
+            Tigre tigre = new Tigre();
+            Gato gato = new Gato();
+
+            //Canino
+            Lobo lobo = new Lobo();
+            Perro perro = new Perro();
+
+
+            MessageBox.Show("FELINOS");
+
+            //Leon
+            leon.setcomer("El leon caza animales para comer");
+            leon.sethacerRuido("Un Leon de gran tamaño le hace guarrrr");
+
+            MessageBox.Show(leon.getAnimals());
+
+            //Tigre
+            tigre.setcomer("El tigre caza animales pequeños para comer carne");
+            tigre.sethacerRuido("Un tigre muy pequeño ruge en la selva");
+
+            MessageBox.Show(tigre.getAnimals());
+
+            //Gato
+            gato.setcomer("El gato come comida Gati, para gatitos");
+            gato.sethacerRuido("Un gatito pequeño y adorale le hace meooowwwww");
+            gato.setVacunar("Al gatito le pusieron la primera vacuna de los 2 meses la parvo-virus");
+
+            MessageBox.Show(gato.getAnimals());
+
+
+            MessageBox.Show("CANINOS");
+
+            //Lobo
+            lobo.setcomer("Los lobos cazan en grupos grandes para comida grande como ciervos, alces, ovejas, cabras, caribúes, bisontes, y muskox");
+            lobo.sethacerRuido("El lobo aullaaa muy fuerte a pesar de ser un lobito chiquito");
+
+            MessageBox.Show(lobo.getAnimals());
+
+            //Perro
+            perro.setcomer("El cachorro come perigri");
+            perro.sethacerRuido("El perro grande ladra mucho por las noches");
+            perro.setVacunar("Al perrito le pusieron la vacuna de la rabia");
+            perro.setsacarPaseo("Al perri lo sacan a pasear para que haga popo");
+
+            MessageBox.Show(perro.getAnimals());
+
+        }
     }
 }
